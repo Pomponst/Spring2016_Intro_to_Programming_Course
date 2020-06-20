de6d4a9a25a6c9308a21bc68d0c79ca1541305bd
Steven Pomponio <63677862+Pomponst@users.noreply.github.com>
2020-06-20 02:05:07 -0400

    Add files via upload

---
+++ Assignment 1/Assignment 1.py
@@ -0,0 +1,23 @@
+costPerShare = 25.625
+
+numberOfShares = 400
+
+amount = costPerShare * numberOfShares
+
+print('The amount is', amount)
+
+price = 19.95
+
+discountPercent = 30
+
+markdown = (discountPercent / 100) * price
+
+price = price - markdown
+
+print('The price is', price)
+
+print('	*   *')
+print('	 * *')
+print('	  *')
+print('	 * *')
+print('	*   *')
